e0484d95fac4dfc5404ddd90831afd0c3b275f8d
dmm <_dmm@mail.ru>
2021-05-05 20:19:47 +0700

    add DOM processing while fetching data

--- docker/db/test.sql
+++ docker/db/test.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS movie (
                                        link varchar(2048) NULL DEFAULT NULL,
                                        description text NULL DEFAULT NULL,
                                        pub_date DATETIME NULL DEFAULT NULL,
-                                       image BLOB NULL DEFAULT NULL
+                                       image varchar(1024) NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
